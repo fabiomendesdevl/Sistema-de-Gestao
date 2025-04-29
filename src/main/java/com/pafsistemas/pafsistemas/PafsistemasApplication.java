0c048688ce14370ceb59858fff9eaa953d4082b2
“fabiomendesdevl” <fabiomendesdev@outlook.com>
2025-04-29 19:48:11 -0300

    new project

---
+++ src/main/java/com/pafsistemas/pafsistemas/PafsistemasApplication.java
@@ -0,0 +1,13 @@
+package com.pafsistemas.pafsistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafsistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafsistemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pafsistemas/pafsistemas/PafsistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pafsistemas.pafsistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafsistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
